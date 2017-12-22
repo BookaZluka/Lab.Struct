5523e526bcb9a5f2d9e0e3bda56a3960d4025b15
BookaZluka <32384558+BookaZluka@users.noreply.github.com>
2017-12-22 11:58:03 +0300

    Update lab7.cpp

--- lab7.cpp
+++ lab7.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
 
+struct Node
+{
+    string str;
+    Node *next;
+};
+
+int main()
+{
+    string str;
+    Node *first = new Node{str, nullptr};
+    Node *last = nullptr;
+    while (true)
+    {
+        cout<<"Введите текст: ";
+        getline(cin, str);
+        if (str == "end") break;
+        Node *curr = new Node{str, nullptr};
+        if (first -> next == nullptr) first -> next = curr;
+        if (last != nullptr)
+            last->next = curr;
+        last = curr;
+    }
+    Node *curr = nullptr;
+    curr = first;
+    do
+    {
+        cout << curr -> str << endl;
+        curr = curr -> next;
+    }
+    while (curr != nullptr);
+    return 0;
+}
